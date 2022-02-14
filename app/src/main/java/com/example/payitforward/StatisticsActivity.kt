3d3af4dd86b8b535ff3f78302157d93fbc34809c
annapotr <57748412+annapotr@users.noreply.github.com>
2022-02-15 01:00:36 +0300

    Init chat

---
+++ app/src/main/java/com/example/payitforward/StatisticsActivity.kt
@@ -0,0 +1,90 @@
+package com.example.payitforward
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.animation.DecelerateInterpolator
+import com.scichart.charting.model.dataSeries.IXyDataSeries
+import com.scichart.charting.visuals.SciChartSurface
+import com.scichart.charting.visuals.axes.IAxis
+import com.scichart.charting.visuals.renderableSeries.FastColumnRenderableSeries
+import com.scichart.charting.visuals.renderableSeries.data.XSeriesRenderPassData
+import com.scichart.charting.visuals.renderableSeries.paletteProviders.IFillPaletteProvider
+import com.scichart.charting.visuals.renderableSeries.paletteProviders.PaletteProviderBase
+import com.scichart.core.framework.UpdateSuspender
+import com.scichart.core.model.IntegerValues
+import com.scichart.drawing.utility.ColorUtil
+import com.scichart.extensions.builders.SciChartBuilder
+import java.util.*
+
+
+class StatisticsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_statistics)
+        initChart()
+    }
+
+    private fun initChart() {
+        val surface: SciChartSurface = findViewById(R.id.surface)
+        SciChartBuilder.init(this)
+        val sciChartBuilder = SciChartBuilder.instance()
+
+        val xAxis: IAxis = sciChartBuilder.newNumericAxis().withGrowBy(0.1, 0.1).build()
+        val yAxis: IAxis = sciChartBuilder.newNumericAxis().withGrowBy(0.0, 0.1).build()
+
+        val dataSeries: IXyDataSeries<Int, Int> = sciChartBuilder.newXyDataSeries(
+            Int::class.java,
+            Int::class.java
+        ).build()
+        val yValues = intArrayOf(50, 35, 61, 58, 50, 50, 40, 53, 55, 23, 45, 12, 59, 60)
+
+        for (i in yValues.indices) {
+            dataSeries.append(i, yValues[i])
+        }
+
+        val rSeries = sciChartBuilder.newColumnSeries()
+            .withStrokeStyle(-0xdcdcdd, 0.4f)
+            .withDataPointWidth(0.7)
+            .withLinearGradientColors(ColorUtil.LightSteelBlue, ColorUtil.SteelBlue)
+            .withDataSeries(dataSeries)
+            .withPaletteProvider(ColumnsPaletteProvider())
+            .build()
+
+        UpdateSuspender.using(surface) {
+            Collections.addAll(surface.xAxes, xAxis)
+            Collections.addAll(surface.yAxes, yAxis)
+            Collections.addAll(surface.renderableSeries, rSeries)
+            Collections.addAll(
+                surface.chartModifiers,
+                sciChartBuilder.newModifierGroupWithDefaultModifiers().build()
+            )
+            sciChartBuilder.newAnimator(rSeries).withWaveTransformation()
+                .withInterpolator(DecelerateInterpolator())
+                .withDuration(3000).withStartDelay(350).start()
+        }
+    }
+}
+
+private class ColumnsPaletteProvider:
+    PaletteProviderBase<FastColumnRenderableSeries>(FastColumnRenderableSeries::class.java),
+    IFillPaletteProvider {
+
+    private val colors = IntegerValues()
+    private val desiredColors = longArrayOf(0xFFa9d34f, 0xFFfc9930, 0xFFd63b3f)
+    override fun update() {
+        val currentRenderPassData = renderableSeries!!.currentRenderPassData as XSeriesRenderPassData
+
+        val size = currentRenderPassData.pointsCount()
+        colors.setSize(size)
+
+        val colorsArray = colors.itemsArray
+        val indices = currentRenderPassData.indices.itemsArray
+        for (i in 0 until size) {
+            val index = indices[i]
+            colorsArray[i] = desiredColors[index % 3].toInt()
+        }
+    }
+
+    override fun getFillColors(): IntegerValues = colors
+
+}
