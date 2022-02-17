729cad3419f5b080c5e257dab6c1717bb49aaab9
annapotr <57748412+annapotr@users.noreply.github.com>
2022-02-17 23:38:52 +0300

    UI of Create Deed Activity

---
+++ app/src/main/java/com/example/payitforward/CreateDeedActivity.kt
@@ -0,0 +1,11 @@
+package com.example.payitforward
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateDeedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_deed)
+    }
+}
