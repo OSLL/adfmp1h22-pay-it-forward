d6ac13ae6381a7c37d15ef050b56b68fa9beba66
Екатерина Шелухина <shelukchina2447@gmail.com>
2022-02-21 00:54:11 +0300

    Add init Menu Navigation

--- app/src/main/java/com/example/payitforward/MainActivity.kt
+++ app/src/main/java/com/example/payitforward/MainActivity.kt
@@ -9,15 +9,27 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     var signUpButton: Button? = null
+    var signInButton: Button? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         signUpButton = findViewById<View>(R.id.signup) as Button
         signUpButton!!.setOnClickListener { signUpActivity() }
+
+
+        signInButton = findViewById<View>(R.id.login_button) as Button
+        signInButton!!.setOnClickListener { signInActivity() }
     }
 
     private fun signUpActivity() {
         val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun signInActivity() {
+        val intent = Intent(this, MenuActivity::class.java)
         startActivity(intent)
     }
 }
