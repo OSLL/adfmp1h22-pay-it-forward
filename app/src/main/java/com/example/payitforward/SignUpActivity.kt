2cd5db3c1c3dee0737df072c9d9e6d726d9ac625
Екатерина Шелухина <shelukchina2447@gmail.com>
2022-02-21 01:41:18 +0300

    Add settings to nav bar

--- app/src/main/java/com/example/payitforward/SignUpActivity.kt
+++ app/src/main/java/com/example/payitforward/SignUpActivity.kt
@@ -9,11 +9,20 @@ import androidx.appcompat.app.AppCompatActivity
 
 class SignUpActivity : AppCompatActivity() {
     var signInButton: Button? = null
+    var signUpButton: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_up)
         signInButton = findViewById<View>(R.id.signin) as Button
         signInButton!!.setOnClickListener { signInActivity() }
+
+        signUpButton = findViewById<View>(R.id.signup_button) as Button
+        signUpButton!!.setOnClickListener { signUpActivity() }
+    }
+
+    private fun signUpActivity() {
+        val intent = Intent(this, MenuActivity::class.java)
+        startActivity(intent)
     }
 
     private fun signInActivity() {
