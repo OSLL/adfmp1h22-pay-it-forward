218e68fe48cf284613aa6fcbb36c0fb0abdc95c5
Екатерина Шелухина <shelukchina2447@gmail.com>
2022-03-10 23:46:19 +0500

    Add cancel buttons to settings

--- app/src/main/java/com/example/payitforward/ChangePasswordActivity.kt
+++ app/src/main/java/com/example/payitforward/ChangePasswordActivity.kt
@@ -2,11 +2,20 @@ package com.example.payitforward
 
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ChangePasswordActivity : AppCompatActivity() {
+    var cancelButton: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_password)
+        cancelButton = findViewById<View>(R.id.button_cancel_change_password) as Button
+        cancelButton!!.setOnClickListener { cancelActivity() }
+    }
+
+    private fun cancelActivity() {
+        finish()
     }
 }
--- app/src/main/java/com/example/payitforward/EditProfileActivity.kt
+++ app/src/main/java/com/example/payitforward/EditProfileActivity.kt
@@ -2,11 +2,22 @@ package com.example.payitforward
 
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
+
 class EditProfileActivity : AppCompatActivity() {
+
+    var cancelButton: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+        cancelButton = findViewById<View>(R.id.button_cancel_edit_profile) as Button
+        cancelButton!!.setOnClickListener { cancelActivity() }
+    }
+
+    private fun cancelActivity() {
+        finish()
     }
 }
