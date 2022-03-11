1d029529bda7e1baa16178f8f415dbff62a6dd76
Екатерина Шелухина <shelukchina2447@gmail.com>
2022-03-12 00:50:50 +0500

    Change home to fragment

--- app/src/main/java/com/example/payitforward/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/payitforward/ui/home/HomeFragment.kt
@@ -1,41 +1,58 @@
 package com.example.payitforward.ui.home
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.payitforward.R
+import com.example.payitforward.adapters.TabsPagerAdapter
+import com.example.payitforward.databinding.ActivityHomeBinding
 import com.example.payitforward.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
 
 class HomeFragment : Fragment() {
 
-private var _binding: FragmentHomeBinding? = null
-  // This property is only valid between onCreateView and
-  // onDestroyView.
-  private val binding get() = _binding!!
+    private lateinit var binding: FragmentHomeBinding
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View {
-    val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-    _binding = FragmentHomeBinding.inflate(inflater, container, false)
-    val root: View = binding.root
-
-    val textView: TextView = binding.textHome
-    homeViewModel.text.observe(viewLifecycleOwner) {
-      textView.text = it
-    }
-    return root
-  }
-
-override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.fragment_home)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+//        setContentView(mBinding.root)
+        val root: View = binding.root
+        binding.tabLayout.setSelectedTabIndicatorColor(Color.WHITE)
+        context?.let { ContextCompat.getColor(it, R.color.pink) }
+            ?.let { binding.tabLayout.setBackgroundColor(it) }
+        binding.tabLayout.tabTextColors =
+            context?.let { ContextCompat.getColorStateList(it, android.R.color.white) }
+        val numberOfTabs = 3
+        binding.tabLayout.tabMode = TabLayout.MODE_FIXED
+        val adapter = TabsPagerAdapter(getParentFragmentManager(), lifecycle, numberOfTabs)
+        binding.tabsViewpager.adapter = adapter
+        TabLayoutMediator(binding.tabLayout, binding.tabsViewpager) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.text = "All"
+                }
+                1 -> {
+                    tab.text = "Get"
+                }
+                2 -> {
+                    tab.text = "Give"
+                }
+            }
+        }.attach()
+        return root
     }
 }
