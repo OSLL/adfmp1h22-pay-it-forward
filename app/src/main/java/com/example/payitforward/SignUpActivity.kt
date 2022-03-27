eacc8904e0d1d8423172446e697d573565aaa49f
Екатерина Шелухина <shelukchina2447@gmail.com>
2022-03-27 22:20:14 +0300

    Some refactoring

--- app/src/main/java/com/example/payitforward/SignUpActivity.kt
+++ app/src/main/java/com/example/payitforward/SignUpActivity.kt
@@ -11,22 +11,19 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.StateSet.TAG
 import com.example.payitforward.pojo.User
+import com.example.payitforward.util.FirestoreUtil
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import com.example.payitforward.util.FirestoreUtil
 
 class SignUpActivity : AppCompatActivity() {
     var signInButton: Button? = null
     var signUpButton: Button? = null
-
     var signUpEmail: EditText? = null
     var signUpPassword: EditText? = null
     var signUpRepeatedPassword: EditText? = null
 
     private val auth: FirebaseAuth = Firebase.auth
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,51 +37,61 @@ class SignUpActivity : AppCompatActivity() {
 
     }
 
-    private fun getUsername(email: String): String {
-        val i = email.indexOf("@")
-        return email.substring(0, i)
-    }
-
     private fun signUpActivity() {
         signUpEmail = findViewById<View>(R.id.login) as EditText
-        val email = signUpEmail!!.text.toString()
-
         signUpPassword = findViewById<View>(R.id.password) as EditText
-        val password = signUpPassword!!.text.toString()
-
         signUpRepeatedPassword = findViewById<View>(R.id.password_repeat) as EditText
+
+        val email = signUpEmail!!.text.toString()
+        val password = signUpPassword!!.text.toString()
         val repeatedPassword = signUpRepeatedPassword!!.text.toString()
 
 
-
         if (password == repeatedPassword) {
-            auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        Log.d(TAG, "createUserWithEmail:success")
-                        val user = auth.currentUser
-
-                        if (user != null) {
-                            FirestoreUtil.addUser(User(user.uid, "", getUsername(email), email, "", ""))
-                        }
-                        val intent = Intent(this, MenuActivity::class.java)
-                        startActivity(intent)
-//                        updateUI(user)
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                        Toast.makeText(
-                            baseContext, "Authentication failed.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-//                        updateUI(null)
-                    }
-                }
+            createUserWithPassword(email, password)
         }
     }
 
+    private fun createUserWithPassword(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    if (user != null) {
+                        FirestoreUtil.addUser(
+                            User(
+                                user.uid,
+                                "",
+                                getUsername(email),
+                                email,
+                                "",
+                                ""
+                            )
+                        )
+                        Log.d(TAG, "createUserWithEmailAndPassword with UUID ${user.uid}:success")
+                    }
+                    runActivity(MenuActivity::class.java)
+                } else {
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed. Please try another password.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+    }
+
     private fun signInActivity() {
-        val intent = Intent(this, MainActivity::class.java)
+        runActivity(MainActivity::class.java)
+    }
+
+    private fun runActivity(loadClass: Class<*>) {
+        val intent = Intent(this, loadClass)
         startActivity(intent)
+    }
+
+    private fun getUsername(email: String): String {
+        val i = email.indexOf("@")
+        return email.substring(0, i)
     }
 }
