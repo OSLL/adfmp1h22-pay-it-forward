76a516c58f9807c6b51427b968230d6fe3b86f15
annapotr <57748412+annapotr@users.noreply.github.com>
2022-04-15 19:23:37 +0300

    Fix test for task

--- app/src/androidTest/java/com/example/payitforward/ItemTaskActivityTest.kt
+++ app/src/androidTest/java/com/example/payitforward/ItemTaskActivityTest.kt
@@ -2,17 +2,13 @@ package com.example.payitforward
 
 import android.content.Intent
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.ViewInteraction
 import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.rule.ActivityTestRule
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import com.google.firebase.Timestamp
-import junit.framework.AssertionFailedError
 import org.junit.Rule
 import org.junit.Test
-import java.util.concurrent.TimeoutException
 
 class ItemTaskActivityTest {
     @Rule
@@ -22,12 +18,12 @@ class ItemTaskActivityTest {
 
     @Test
     fun testTaskWithCompletedType() {
-        var intent = Intent(Intent.ACTION_PICK)
+        val intent = Intent(Intent.ACTION_PICK)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         intent.putExtra("taskId", "K5pdckS2mXTjZWs8JQZyIIWR2Ks21650022149")
         intent.putExtra("creationDate", "")
-        intent.putExtra("deadlineDate",  Timestamp.now().seconds)
+        intent.putExtra("deadlineDate", Timestamp.now().seconds)
         intent.putExtra("authorId", "K5pdckS2mXTjZWs8JQZyIIWR2Ks2")
         intent.putExtra("completedId", "6SSb9c9J98SMCWRMtkrkyHyGYPP2")
         intent.putExtra("name", "Choose a gift")
@@ -36,32 +32,28 @@ class ItemTaskActivityTest {
         intent.putExtra("coins", 65)
         intent.putExtra("taskType", "completed")
         activityRule.launchActivity(intent)
-      //  activityRule.runOnUiThread {
-        /*
-           onView(ViewMatchers.withId(R.id.taskImageView))
+        onView(ViewMatchers.withId(R.id.taskImageView))
             .check(ViewAssertions.matches(isDisplayed()))
-            onView(ViewMatchers.withId(R.id.taskName))
-                .check(ViewAssertions.matches(isDisplayed()))
-            onView(ViewMatchers.withId(R.id.deadlineTextView))
-                .check(ViewAssertions.matches(isDisplayed()))
-            onView(ViewMatchers.withId(R.id.deadlineDate))
-                .check(ViewAssertions.matches(isDisplayed()))
-            onView(ViewMatchers.withId(R.id.authorNameTextView))
-                .check(ViewAssertions.matches(isDisplayed()))
-            onView(ViewMatchers.withId(R.id.authorTextView))
-                .check(ViewAssertions.matches(isDisplayed()))
-            onView(ViewMatchers.withId(R.id.descriptionTextView))
-                .check(ViewAssertions.matches(isDisplayed()))
-            onView(ViewMatchers.withId(R.id.coinsImageView))
-                .check(ViewAssertions.matches(isDisplayed()))
-            onView(ViewMatchers.withId(R.id.coinsTextView))
-                .check(ViewAssertions.matches(isDisplayed()))
-            onView(ViewMatchers.withId(R.id.coins))
-                .check(ViewAssertions.matches(isDisplayed()))*/
-        }
-        //activityRule.finishActivity()
-       // onView(ViewMatchers.withId(R.id.buttonEdit))
-       //    .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-
+        onView(ViewMatchers.withId(R.id.taskName))
+            .check(ViewAssertions.matches(isDisplayed()))
+        onView(ViewMatchers.withId(R.id.deadlineTextView))
+            .check(ViewAssertions.matches(isDisplayed()))
+//        onView(ViewMatchers.withId(R.id.deadlineDate))
+//            .check(ViewAssertions.matches(isDisplayed()))
+//        onView(ViewMatchers.withId(R.id.authorNameTextView))
+//            .check(ViewAssertions.matches(isDisplayed()))
+        onView(ViewMatchers.withId(R.id.authorTextView))
+            .check(ViewAssertions.matches(isDisplayed()))
+//        onView(ViewMatchers.withId(R.id.descriptionTextView))
+//            .check(ViewAssertions.matches(isDisplayed()))
+        onView(ViewMatchers.withId(R.id.coinsImageView))
+            .check(ViewAssertions.matches(isDisplayed()))
+        onView(ViewMatchers.withId(R.id.coinsTextView))
+            .check(ViewAssertions.matches(isDisplayed()))
+//        onView(ViewMatchers.withId(R.id.coins))
+//            .check(ViewAssertions.matches(isDisplayed()))
+         onView(ViewMatchers.withId(R.id.buttonEdit))
+            .check(ViewAssertions.matches(isDisplayed()))
     }
+
 }
