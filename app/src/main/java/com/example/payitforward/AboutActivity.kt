1e413606d36bfffe0e4a4772a1b142386b5a4900
Екатерина Шелухина <shelukchina2447@gmail.com>
2022-04-26 19:12:29 +0300

    Fix about info

--- app/src/main/java/com/example/payitforward/AboutActivity.kt
+++ app/src/main/java/com/example/payitforward/AboutActivity.kt
@@ -4,16 +4,17 @@ package com.example.payitforward
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class AboutActivity : AppCompatActivity() {
 
-    var cancelButton: Button? = null
+    var cancelButton: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
-        cancelButton = findViewById<View>(R.id.button_cancel_about) as Button
+        cancelButton = findViewById<View>(R.id.back_button) as ImageButton
         cancelButton!!.setOnClickListener { cancelActivity() }
     }
 
